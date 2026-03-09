69b48d237ffff573239ee6c07dc86796194bd20a
Ramsis Magdy <ramsismagdy@gmail.com>
2026-03-09 10:58:39 +0200

    test deployment

--- app/page.js
+++ app/page.js
@@ -1,17 +1,7 @@
 export default function Home() {
   return (
-    <main style={{padding:"40px",fontFamily:"Arial"}}>
-      <h1>Alfaguides Dental Lab Portal</h1>
-
-      <p>Welcome to the dental case management system.</p>
-
-      <div style={{marginTop:"30px"}}>
-        <a href="/login">Login</a>
-      </div>
-
-      <div style={{marginTop:"10px"}}>
-        <a href="/new-case">Submit New Case</a>
-      </div>
-    </main>
+    <div style={{padding:"50px"}}>
+      <h1>ALFAGUIDES SYSTEM WORKING</h1>
+    </div>
   )
 }
